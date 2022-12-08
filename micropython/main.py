49d5be93312a6b10e623ffa125dd80450e5082e8
Kazuki Yamamoto <k.yamamoto.08136891@gmail.com>
2022-12-08 21:28:38 +0900

    update main.py to use AQM0802 lcd GpakMux

--- micropython/main.py
+++ micropython/main.py
@@ -1,15 +1,17 @@
 import time
 import sys
 
-from machine import I2C, Pin
+from machine import I2C, Pin, PWM
 
-from KeysightU2751AEmu import KeysightU2751AEmu
+import GpakMux
+from EMU2751A import EMU2751A
+from AQM0802 import AQM0802
 
 led = Pin(25, Pin.OUT)
 getc = sys.stdin.read
 gets = sys.stdin.readline
 putc = sys.stdout.write
-u2751a = KeysightU2751AEmu()
+u2751a = EMU2751A()
 
 
 def main():
@@ -52,5 +54,27 @@ def shell():
             u2751a.parse_and_process(line)
 
 
+bus = I2C(1, scl=Pin(3), sda=Pin(2))
+
 if __name__ == '__main__':
+
+    rst = Pin(17, mode=Pin.OUT, value=1)
+    backlight = PWM(Pin(14, mode=Pin.OUT))
+    aqm = AQM0802(bus, backlight)
+
+    for c in "Hello, World!".encode("utf8"):
+        aqm.send_data(int(c))
+
+    for i in range(4):
+        if GpakMux.SLG46826_ADDR | (i << 6) in bus.scan():
+            mux = GpakMux.GpakMux(bus, i)
+            break
+    else:
+        mux = None
+        print("GpakMux not found!")
+
+    if mux is not None:
+        for row in [1, 2]:
+            for col in [1, 2, 3, 4, 5, 6]:
+                mux.connect(row, col)
     shell()
