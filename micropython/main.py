fb6ea2a6ac63c95ffa58b2c83f66fc578678ceaa
K4zuki <k.yamamoto.08136891@gmail.com>
2022-12-11 20:14:30 +0900

    update main.py

--- micropython/main.py
+++ micropython/main.py
@@ -5,7 +5,9 @@ from machine import I2C, Pin, PWM
 
 import GpakMux
 from EMU2751A import EMU2751A
-from AQM0802 import AQM0802
+from AQM0802 import AQM0802, AQM0802_ADDR
+from SO1602OLED import SO1602OLED, SO1602A_ADDR
+from hd44780compat import BitField, Register
 
 led = Pin(25, Pin.OUT)
 getc = sys.stdin.read
@@ -54,27 +56,32 @@ def shell():
             u2751a.parse_and_process(line)
 
 
-bus = I2C(1, scl=Pin(3), sda=Pin(2))
+bus = I2C(1, scl=Pin(3), sda=Pin(2), freq=60_000)
 
 if __name__ == '__main__':
 
-    rst = Pin(17, mode=Pin.OUT, value=1)
-    backlight = PWM(Pin(14, mode=Pin.OUT))
-    aqm = AQM0802(bus, backlight)
+    if AQM0802_ADDR in bus.scan():
+        rst = Pin(17, mode=Pin.OUT, value=1)
+        backlight = PWM(Pin(14, mode=Pin.OUT))
+        aqm = AQM0802(bus, backlight)
 
-    for c in "Hello, World!".encode("utf8"):
-        aqm.send_data(int(c))
+        for c in "Hello,\nWor\nld!".encode("utf8"):
+            aqm.send_data(int(c))
+
+    if SO1602A_ADDR in bus.scan():
+        oled = SO1602OLED(bus)
+
+        for c in "Hello,\nWor\nld!".encode("utf8"):
+            oled.send_data(int(c))
 
     for i in range(4):
-        if GpakMux.SLG46826_ADDR | (i << 6) in bus.scan():
+        if GpakMux.SLG46826_ADDR | (i << 5) in bus.scan():
+            print(f"GpakMux #{i} found")
             mux = GpakMux.GpakMux(bus, i)
-            break
-    else:
-        mux = None
-        print("GpakMux not found!")
 
-    if mux is not None:
-        for row in [1, 2]:
-            for col in [1, 2, 3, 4, 5, 6]:
-                mux.connect(row, col)
+            for row in [1, 2]:
+                for col in [1, 2, 3, 4, 5, 6]:
+                    mux.connect(row, col)
+                    time.sleep(0.01)
+
     shell()
