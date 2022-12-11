dfcc44531ba42e80b7f3b6713da07e67c794c080
K4zuki <k.yamamoto.08136891@gmail.com>
2022-12-11 15:36:31 +0900

    update AQM0802.py
    - imports `hd44780compat`, expands `HD44780Instructions` to handle extended instructions
    - add vis`ible and `line_counter` variables
    - add `line_feed()` function
    - update `set_backlight()` to avoid blackout at 100% duty.

--- micropython/AQM0802.py
+++ micropython/AQM0802.py
@@ -1,14 +1,103 @@
 from machine import I2C, Pin, PWM
 
+from hd44780compat import BitField, Register, Function, HD44780Instructions
+
+CR = 0x0d
+LF = 0x0a
 AQM0802_ADDR = 0x3e  # 7-bits
 
 
+class AQM0802Function(Function):
+    INSTRUCTION_TABLE = BitField("INSTRUCTION_TABLE", 1, 0, 0b00, False)
+
+    @property
+    def fields(self):
+        return (self.INSTRUCTION_TABLE,
+                BitField("CONST", 1, 1, 0b00, True),
+                self.FONT_SELECTION,
+                self.DISPLAY_LINES_NUMBER,
+                self.DATA_LENGTH,
+                BitField("CONST", 3, 5, 0b001, True),
+                )
+
+
+class InternalOscillator(Register):
+    ADJUST = BitField("ADJUST", 3, 0, 0b000, False)
+    BIAS = BitField("BIAS", 1, 3, 0b0, False)
+
+    @property
+    def fields(self):
+        return (self.ADJUST,
+                self.BIAS,
+                BitField("CONST", 4, 4, 0b0001, True),
+                )
+
+
+class ICONAddress(Register):
+    AC = BitField("AC", 4, 0, 0b0000, False)
+
+    @property
+    def fields(self):
+        return (self.AC,
+                BitField("CONST", 4, 4, 0b0100, True),
+                )
+
+
+class PowerControl(Register):
+    CONTRAST_MSB = BitField("CONTRAST_MSB", 2, 0, 0b00, False)
+    BOOST_STATUS = BitField("BOOST_STATUS", 1, 2, 0b0, False)
+    ICON_STATUS = BitField("ICON_STATUS", 1, 3, 0b0, False)
+
+    @property
+    def fields(self):
+        return (self.CONTRAST_MSB,
+                self.BOOST_STATUS,
+                self.ICON_STATUS,
+                BitField("CONST", 4, 4, 0b0101, True),
+                )
+
+
+class FollowerControl(Register):
+    FOLLOWER_STATUS = BitField("FOLLOWER_STATUS", 1, 3, 0b0, False)
+    AMPL_RATIO = BitField("AMPL_RATIO", 3, 0, 0b000, False)
+
+    @property
+    def fields(self):
+        return (self.AMPL_RATIO,
+                self.FOLLOWER_STATUS,
+                BitField("CONST", 4, 4, 0b0110, True),
+                )
+
+
+class ContrastSet(Register):
+    CONTRAST_LSB = BitField("CONTRAST_LSB", 4, 0, 0b0000, False)
+
+    @property
+    def fields(self):
+        return (self.CONTRAST_LSB,
+                BitField("CONST", 4, 4, 0b0111, True),
+                )
+
+
+class AQM0802Instructions(HD44780Instructions):
+    Function = AQM0802Function()
+    InternalOscillator = InternalOscillator()
+    ICONAddress = ICONAddress()
+    PowerControl = PowerControl()
+    FollowerControl = FollowerControl()
+    ContrastSet = ContrastSet()
+
+
 class AQM0802:
+    slave_address = AQM0802_ADDR
     bus = None
     bled = None
-    reset = None
+    rst = None
+    visible = False
+    line_counter = 0x00
+    instructions = AQM0802Instructions()
 
-    def __init__(self, bus, bled, reset=None):
+    def __init__(self, bus, bled=None, reset=None):
         """
 
         :param I2C bus:
@@ -17,33 +106,83 @@ class AQM0802:
         """
         self.bus = bus
         self.bled = bled
-        self.reset = reset
+        self.rst = reset
 
-        if AQM0802_ADDR in self.bus.scan():
-            self._init_lcd()
+        if self.slave_address in self.bus.scan():
+            self.visible = True
+            self._startup()
 
-    def _init_lcd(self):
-        self.send_command(0x38)
-        self.send_command(0x39)
-        self.send_command(0x14)
-        self.send_command(0x70)
-        self.send_command(0x56)
-        self.send_command(0x6c)
-        self.send_command(0x38)
-        self.send_command(0x0c)
-        self.send_command(0x01)
-        self.set_backlight(50)
+        self.set_backlight(30)
+
+    def _startup(self):
+        self.instructions.Function.INSTRUCTION_TABLE.val = 0
+        self.instructions.Function.DISPLAY_LINES_NUMBER.val = 1
+        self.instructions.Function.DATA_LENGTH.val = 1
+        self.send_command(self.instructions.Function.val)
+        # self.send_command(0x38)
+
+        self.instructions.Function.INSTRUCTION_TABLE.val = 1
+        self.send_command(self.instructions.Function.val)
+        # self.send_command(0x39)
+
+        self.instructions.InternalOscillator.BIAS.val = 0
+        self.instructions.InternalOscillator.ADJUST.val = 4
+        self.send_command(self.instructions.InternalOscillator.val)
+        # self.send_command(0x14)
+
+        self.instructions.ContrastSet.CONTRAST_LSB.val = 0
+        self.send_command(self.instructions.ContrastSet.val)
+        # self.send_command(0x70)
+
+        self.instructions.PowerControl.BOOST_STATUS.val = 1
+        self.instructions.PowerControl.CONTRAST_MSB.val = 2
+        self.send_command(self.instructions.PowerControl.val)
+        # self.send_command(0x56)
+
+        self.instructions.FollowerControl.FOLLOWER_STATUS.val = 1
+        self.instructions.FollowerControl.AMPL_RATIO.val = 4
+        self.send_command(self.instructions.FollowerControl.val)
+        # self.send_command(0x6c)
+
+        self.instructions.Function.INSTRUCTION_TABLE.val = 0
+        self.send_command(self.instructions.Function.val)
+        # self.send_command(0x38)
+
+        self.instructions.DisplayStatus.DISPLAY_STATUS.val = 1
+        self.send_command(self.instructions.DisplayStatus.val)
+        # self.send_command(0x0c)
+
+        self.send_command(self.instructions.ClearDisplay.val)
+        # self.send_command(0x01)
+
+    def reset(self):
+        if self.rst is not None:
+            self.rst.off()
+            self.rst.on()
+        self._startup()
 
     def send_command(self, command):
-        self.bus.writeto(AQM0802_ADDR, bytes([0x00, command]))
+        if self.visible:
+            self.bus.writeto(self.slave_address, bytes([0x00, command]))
         return
 
     def send_data(self, data):
-        self.bus.writeto(AQM0802_ADDR, bytes([0x40, data]))
+        if data == LF:
+            self.line_feed()
+            self.send_command(0x80 | (self.line_counter << 5))
+        elif data == CR:
+            pass
+        else:
+            if self.visible:
+                self.bus.writeto(self.slave_address, bytes([0x40, data]))
         return
+
+    def line_feed(self):
+        self.line_counter += 1
+        self.line_counter &= 0x01
 
     def set_backlight(self, duty=50):
         max_duty = 65535
         if self.bled is not None:
-            duty = min(max_duty, int(duty * 65536 // 100))
+            duty = min(max_duty - 1, int(duty * max_duty / 100))  # avoid blackout when set at 100% duty
             self.bled.duty_u16(duty)
