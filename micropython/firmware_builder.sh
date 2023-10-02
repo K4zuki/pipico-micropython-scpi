a5b5df03349daaaf0c6d6112d94c1f3fb61ecc03
Kazuki Yamamoto <k.yamamoto.08136891@gmail.com>
2023-10-02 23:16:33 +0900

    Update firmware_builder.sh
    mkdir -p

--- micropython/firmware_builder.sh
+++ micropython/firmware_builder.sh
@@ -3,5 +3,5 @@
 MANIFEST=/root/manifest.py
 make -C ports/rp2 -j6 BOARD=RPI_PICO FROZEN_MANIFEST=$MANIFEST clean
 make -C ports/rp2 -j6 BOARD=RPI_PICO FROZEN_MANIFEST=$MANIFEST clean all
-mkdir /root/build
+mkdir -p /root/build
 mv /raspberrypi-pico/micropython/ports/rp2/build-RPI_PICO/firmware.uf2 /root/build/pipico-micropython-scpi.uf2
