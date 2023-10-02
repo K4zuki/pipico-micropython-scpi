bdd215599608cb97a8beb6e8c452b4edc967b98d
K4zuki <k.yamamoto.08136891@gmail.com>
2023-10-02 11:15:27 +0900

    update firmware_builder.sh - moves uf2 file into build/ directory

--- micropython/firmware_builder.sh
+++ micropython/firmware_builder.sh
@@ -3,4 +3,5 @@
 MANIFEST=/root/manifest.py
 make -C ports/rp2 -j6 BOARD=RPI_PICO FROZEN_MANIFEST=$MANIFEST clean
 make -C ports/rp2 -j6 BOARD=RPI_PICO FROZEN_MANIFEST=$MANIFEST clean all
-mv /raspberrypi-pico/micropython/ports/rp2/build-RPI_PICO/firmware.uf2 /root/pipico-micropython-scpi.uf2
+mkdir /root/build
+mv /raspberrypi-pico/micropython/ports/rp2/build-RPI_PICO/firmware.uf2 /root/build/pipico-micropython-scpi.uf2
