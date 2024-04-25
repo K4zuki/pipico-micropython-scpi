e26559bc351aa9c23ff6a42bd3d3ea8387b40991
Kazuki Yamamoto <k.yamamoto.08136891@gmail.com>
2024-04-25 20:03:48 +0900

    gha challenge: echo env

--- micropython/firmware_builder.sh
+++ micropython/firmware_builder.sh
@@ -5,3 +5,4 @@ make -C ports/rp2 -j6 BOARD=RPI_PICO FROZEN_MANIFEST=$MANIFEST clean
 make -C ports/rp2 -j6 BOARD=RPI_PICO FROZEN_MANIFEST=$MANIFEST clean all
 mkdir -p /root/build
 mv /raspberrypi-pico/micropython/ports/rp2/build-RPI_PICO/firmware.uf2 /root/build/pipico-micropython-scpi.uf2
+echo $(REF)
