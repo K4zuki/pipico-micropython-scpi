d04f05c50fb00cee7ac6e5573b60a09110eca745
Kazuki Yamamoto <k.yamamoto.08136891@gmail.com>
2026-01-21 11:36:10 +0900

    update usb488test.py - change PID to 0x488

--- micropython/usb488test.py
+++ micropython/usb488test.py
@@ -12,6 +12,7 @@ from usb488 import Usb488Interface
 usb488 = Usb488Interface()
 
 usb.device.get().init(usb488,
+                      id_product=0x0488,
                       builtin_driver=True,
                       max_power_ma=100,
                       manufacturer_str="MicroPython",
