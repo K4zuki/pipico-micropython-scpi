8fff86a6e388be2a9c989339a1dbc76db2b053a7
Kazuki Yamamoto <k.yamamoto.08136891@gmail.com>
2026-01-24 23:12:35 +0900

    update manifest.py - freeze tmc.py and usb488.py

--- micropython/manifest.py
+++ micropython/manifest.py
@@ -1,6 +1,8 @@
 include("$(MPY_DIR)/ports/rp2/boards/manifest.py")
 require("usb-device")
 
+module("usb/device/tmc.py")
+module("usb/device/usb488.py")
 module("MicroScpiDevice.py")
 module("RaspberryScpiPico.py")
 module("main.py")
