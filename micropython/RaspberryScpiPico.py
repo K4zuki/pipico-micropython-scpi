e910c4e31b49cd9b093bd6f42efa07cfce36e81f
Kazuki Yamamoto <k.yamamoto.08136891@gmail.com>
2026-02-13 06:41:46 +0900

    update MicroScpiDevice.py - fix RaspberryScpiPico.py - remove ENDCHAR constant

--- micropython/RaspberryScpiPico.py
+++ micropython/RaspberryScpiPico.py
@@ -160,8 +160,6 @@ E_INVALID_PARAMETER = ScpiErrorNumber(-224, "Invalid parameter value")
 E_I2C_FAIL = ScpiErrorNumber(-333, "I2C bus error")
 E_SPI_FAIL = ScpiErrorNumber(-334, "SPI bus error")
 
-ENDCHAR = const("\n")
-
 pin0 = machine.Pin(0, mode=machine.Pin.OUT, value=IO_ON)  # no-error indicator
 pin1 = machine.Pin(1, mode=machine.Pin.OUT, value=IO_OFF)  # error indicator
 
