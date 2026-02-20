1d404c8b15c5c53757b9a4bdad298b667df0f96d
Kazuki Yamamoto <k.yamamoto.08136891@gmail.com>
2026-02-20 16:58:13 +0900

    update tmc manifest.py - change library load order and path

--- micropython/tmc/manifest.py
+++ micropython/tmc/manifest.py
@@ -1,9 +1,9 @@
 include("$(MPY_DIR)/ports/rp2/boards/manifest.py")
 require("usb-device")
 
-module("RaspberryScpiPico.py", base_path="../")
-module("tmc.py", base_path="../")
-module("usb488.py", base_path="../")
 module("MicroScpiDevice.py", base_path="../")
-module("Usb488ScpiPico.py", base_path="../")
+module("RaspberryScpiPico.py", base_path="../")
+module("tmc.py")
+module("usb488.py")
+module("Usb488ScpiPico.py")
 module("main.py")
