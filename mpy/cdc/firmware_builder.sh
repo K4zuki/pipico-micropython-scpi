312665747df55f6eae1abec6e3d817b4397a5457
Kazuki Yamamoto <k.yamamoto.08136891@gmail.com>
2026-02-28 19:02:48 +0900

    update cdc|tmc/firmware_builder.sh - mpy/ to micropython/

--- mpy/cdc/firmware_builder.sh
+++ mpy/cdc/firmware_builder.sh
@@ -4,5 +4,5 @@ MANIFEST=/root/cdc/manifest.py
 make -C ports/rp2 -j6 BOARD=RPI_PICO FROZEN_MANIFEST=$MANIFEST clean
 make -C ports/rp2 -j6 BOARD=RPI_PICO FROZEN_MANIFEST=$MANIFEST clean all
 mkdir -p /root/build
-mv /raspberrypi-pico/mpy/ports/rp2/build-RPI_PICO/firmware.uf2 /root/build/pipico-micropython-scpi-cdc.uf2
+mv /raspberrypi-pico/micropython/ports/rp2/build-RPI_PICO/firmware.uf2 /root/build/pipico-micropython-scpi-cdc.uf2
 echo $(REF)
--- mpy/tmc/firmware_builder.sh
+++ mpy/tmc/firmware_builder.sh
@@ -4,5 +4,5 @@ MANIFEST=/root/tmc/manifest.py
 make -C ports/rp2 -j6 BOARD=RPI_PICO FROZEN_MANIFEST=$MANIFEST clean
 make -C ports/rp2 -j6 BOARD=RPI_PICO FROZEN_MANIFEST=$MANIFEST clean all
 mkdir -p /root/build
-mv /raspberrypi-pico/mpy/ports/rp2/build-RPI_PICO/firmware.uf2 /root/build/pipico-micropython-scpi.uf2
+mv /raspberrypi-pico/micropython/ports/rp2/build-RPI_PICO/firmware.uf2 /root/build/pipico-micropython-scpi.uf2
 echo $(REF)
